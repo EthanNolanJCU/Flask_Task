f702da17a0d9874e48e5e24713f4783a99c761c6
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-11-30 18:13:36 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=''):
+    return f'Hello {name}'
+
+@app.route('/f')
+@app.route('/f/<temp>')
+def temp(temp=0):
+    return f'{ctof(float(temp))}'
+
+
+def ctof(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+if __name__ == '__main__':
+    app.run()
